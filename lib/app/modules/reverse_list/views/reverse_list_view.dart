5d21048b7da18c02ec46ac3b8d1b0bde11731a16
wlDev <joefachrizal@gmail.com>
2023-04-16 15:38:03 +0700

    implement chunk data from array data, and show to gridview

--- lib/app/modules/reverse_list/views/reverse_list_view.dart
+++ lib/app/modules/reverse_list/views/reverse_list_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,6 +8,7 @@ import '../controllers/reverse_list_controller.dart';
 
 class ReverseListView extends GetView<ReverseListController> {
   const ReverseListView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +16,95 @@ class ReverseListView extends GetView<ReverseListController> {
         title: const Text('ReverseListView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ReverseListView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: SafeArea(
+          child: SizedBox(
+        width: 400,
+        height: 320,
+        child: buildListViewX(),
+      )),
+    );
+  }
+
+  buildGridView() {
+    return GridView.count(
+        crossAxisCount: 4,
+        mainAxisSpacing: 2,
+        crossAxisSpacing: 2,
+        scrollDirection: Axis.horizontal,
+        reverse: true,
+        padding: const EdgeInsets.all(16),
+        childAspectRatio: 1,
+        children: List.generate(
+          controller.counter,
+          (index) => Card(
+            color: Colors.primaries[Random().nextInt(Colors.primaries.length)]
+                [Random().nextInt(9) * 100],
+            child: Center(
+                child: Text(
+              "${controller.numeric1[index]}",
+              style: const TextStyle(color: Colors.black38, fontSize: 20),
+            )),
+          ),
+        ));
+  }
+
+  buildListViewX() {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: controller.numeric2.length,
+      scrollDirection: Axis.horizontal,
+      reverse: true,
+      itemBuilder: (context, position) {
+        return Obx(() {
+          return SizedBox(
+            width: 70,
+            height: 70,
+            child: buildListViewY(
+              controller.numeric2[position],
+              controller.isReverses[position],
+            ),
+          );
+        });
+      },
+    );
+  }
+
+  buildColumnY(numeric2, int position) {
+    return Container(
+        color: Colors.deepOrange,
+        child: Column(
+          children: [
+            Card(child: Container(
+              color: Colors.blue,
+              height: 70,
+              child: Center(child: Text(
+                "${numeric2[position]}",
+                style: const TextStyle(color: Colors.black38, fontSize: 20),
+              )),
+            )),
+          ],
+        ));
+  }
+
+  buildListViewY(numeric2, isReversed) {
+    return Container(
+      alignment: isReversed ? const Alignment(0, 1) : Alignment.bottomCenter,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        reverse: true,
+        itemCount: numeric2.length,
+        itemBuilder: (context, position) {
+          return Card(child: Container(
+            color: Colors.redAccent,
+            height: 70,
+            child: Center(child: Text(
+              "${numeric2[position]}",
+              style: const TextStyle(color: Colors.black38, fontSize: 20),
+            )),
+          ));
+        },
       ),
     );
   }
